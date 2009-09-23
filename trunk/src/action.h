56b3f9974745bdc405bd1896479057fa4ab2e3c2
kometbomb <kometbomb@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2009-09-23 10:48:46 +0000

    Added license to source files + fixed typos

--- trunk/src/action.h
+++ trunk/src/action.h
@@ -1,5 +1,30 @@
-#ifndef ACTIONS_H
-#define ACTIONS_H
+#ifndef ACTION_H
+#define ACTION_H
+
+/*
+Copyright (c) 2009 Tero Lindeman (kometbomb)
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 void select_sequence_position(void *channel, void *position, void *);
 void select_pattern_param(void *id, void *position, void *pattern);
