0d4da386f83d9d97fa534a2f0a13859a38a70788
kometbomb <kometbomb@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2009-09-24 11:56:26 +0000

    Refactored actions

--- src/action.c
+++ src/action.c
@@ -47,4 +47,67 @@ void select_pattern_param(void *id, void *position, void *pattern)
 void select_instrument(void *idx, void *unused1, void *unused2)
 {
 	mused.current_instrument = (int)idx;
+	if (mused.current_instrument >= NUM_INSTRUMENTS) mused.current_instrument = NUM_INSTRUMENTS-1;
+	else if (mused.current_instrument < 0) mused.current_instrument = 0;
+}
+
+
+void change_octave(void *delta, void *unused1, void *unused2)
+{
+	mused.octave += (int)delta;
+	if (mused.octave > 7) mused.octave = 7;
+	else if (mused.octave < 0) mused.octave = 0;
+}
+
+
+void change_song_rate(void *delta, void *unused1, void *unused2)
+{
+	if ((int)delta > 0 && (int)mused.song.song_rate + (int)delta <= 0xff)
+		mused.song.song_rate += (int)delta;
+	else if ((int)delta < 0 && (int)mused.song.song_rate + (int)delta >= 0x1)
+		mused.song.song_rate += (int)delta;
+	cyd_set_callback(&mused.cyd, mus_advance_tick, &mused.mus, mused.song.song_rate);
+}
+
+
+void change_time_signature(void *beat, void *unused1, void *unused2)
+{
+	if (!beat)
+	{
+		mused.time_signature = (mused.time_signature & 0x00ff) | (((((mused.time_signature >> 8) + 1) & 0xff) % 17) << 8);
+		if ((mused.time_signature & 0xff00) == 0) mused.time_signature |= 0x100;
+	}
+	else
+	{
+		mused.time_signature = (mused.time_signature & 0xff00) | ((((mused.time_signature & 0xff) + 1) & 0xff) % 17);
+		if ((mused.time_signature & 0xff) == 0) mused.time_signature |= 1;
+	}
+}
+
+
+void play(void *from, void *unused1, void *unused2)
+{
+	cyd_set_callback(&mused.cyd, mus_advance_tick, &mused.mus, mused.song.song_rate);
+	mus_set_song(&mused.mus, &mused.song, (int)from);
+}
+
+
+void stop(void *unused1, void *unused2, void *unused3)
+{
+	mus_set_song(&mused.mus, NULL, 0);
+}
+
+
+void change_song_speed(void *speed, void *delta, void *unused)
+{
+	if (!speed)
+	{
+		if ((int)mused.song.song_speed + (int)delta > 1 && (int)mused.song.song_speed + (int)delta < 255)
+			mused.song.song_speed += (int)delta;
+	}
+	else
+	{
+		if ((int)mused.song.song_speed2 + (int)delta > 1 && (int)mused.song.song_speed2 + (int)delta < 255)
+		mused.song.song_speed2 += (int)delta;
+	}
 }
