0a6f74bb1b6466fd365065a2f6a0790572793a87
kometbomb <kometbomb@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2009-12-13 21:01:00 +0000

    Fixed filebox gfx not set

--- trunk/src/mused.c
+++ trunk/src/mused.c
@@ -259,14 +259,18 @@ void init(MusInstrument *instrument, MusPattern *pattern, MusSeqPattern sequence
 	
 	change_mode(EDITSEQUENCE);
 	
+	debug("init done");
+}
+
+
+void init_scrollbars()
+{
 	slider_set_params(&mused.sequence_slider_param, 0, 0, 0, 0, &mused.sequence_position, 1, SLIDER_VERTICAL, mused.slider_bevel);
 	slider_set_params(&mused.pattern_slider_param, 0, 0, 0, 0, &mused.sequence_position, 1, SLIDER_VERTICAL, mused.slider_bevel);
 	slider_set_params(&mused.instrument_list_slider_param, 0, 0, 0, 0, &mused.sequence_position, 1, SLIDER_VERTICAL, mused.slider_bevel);
 	slider_set_params(&mused.pattern_horiz_slider_param, 0, 0, 0, 0, &mused.sequence_position, 1, SLIDER_HORIZONTAL, mused.slider_bevel);
 	slider_set_params(&mused.sequence_horiz_slider_param, 0, 0, 0, 0, &mused.sequence_position, 1, SLIDER_HORIZONTAL, mused.slider_bevel);
 	slider_set_params(&mused.program_slider_param, 0, 0, 0, 0, &mused.sequence_position, 1, SLIDER_VERTICAL, mused.slider_bevel);
-	
-	debug("init done");
 }
 
 
