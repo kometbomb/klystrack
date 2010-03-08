ee308f99cb2eebf17a3850f56e58be703b608a29
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2010-03-08 18:01:30 +0000

    AHX import sketch

--- trunk/src/import.c
+++ trunk/src/import.c
@@ -34,7 +34,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 extern Mused mused;
 extern GfxDomain *domain;
 
-static Uint16 find_command(Uint16 command)
+static Uint16 find_command_pt(Uint16 command)
 {
 	if ((command & 0xff00) == 0x0c00)
 		command = 0x0c00 | ((command & 0xff) * 2);
@@ -50,6 +50,74 @@ static Uint16 find_command(Uint16 command)
 		command = 0;	
 	
 	return command;
+}
+
+
+static Uint16 find_command_ahx(Uint8 command, Uint8 data, Uint8 *ctrl)
+{
+	if (command == 0xc)
+	{
+		if (data <= 0x40)
+			return 0xC00 | (data * 2);
+	}
+	else if (command == 0xa)
+	{
+		return 0xa00 | data;
+	}
+	else if (command == 0x3)
+	{
+		return 0x300|(data * 4);
+	}
+	else if (command == 0x5)
+	{
+		*ctrl |= MUS_CTRL_SLIDE|MUS_CTRL_LEGATO;
+		return 0xa00 | data;
+	}
+	else if (command == 0x1)
+	{
+		return 0x100 | (data * 4);
+	}
+	else if (command == 0x2)
+	{
+		return 0x200 | (data * 4);
+	}
+	else if (command == 0xf)
+	{
+		return 0xf00 | (data & 0xf);
+	}
+	else if (command == 0xe)
+	{
+		if ((data & 0xf0) == 0xc0)
+		{
+			return 0xec0|(data&0xf);
+		}
+		else if ((data & 0xf0) == 0xd0)
+		{
+			return 0xed0|(data&0xf);
+		}
+		else if ((data & 0xf0) == 0x40)
+		{
+			return 0x400|(data&0xf);
+		}
+		else if ((data & 0xf0) == 0x10)
+		{
+			return 0xe10|((data&0xf));
+		}
+		else if ((data & 0xf0) == 0x20)
+		{
+			return 0xe20|((data&0xf));
+		}
+		else if ((data & 0xf0) == 0xa0)
+		{
+			return 0xeb0|((data&0xf));
+		}
+		else if ((data & 0xf0) == 0xb0)
+		{
+			return 0xea0|((data&0xf));
+		}
+	}
+	
+	return 0;
 }
 
 static Uint8 find_note(Uint16 period)
@@ -164,7 +232,7 @@ static int import_mod(FILE *f)
 				
 				mused.song.pattern[pat].step[s].note = find_note(SDL_SwapBE16(period) & 0xfff);
 				mused.song.pattern[pat].step[s].instrument = ((inst >> 4) | ((SDL_SwapBE16(period) & 0xf000) >> 8)) - 1;
-				mused.song.pattern[pat].step[s].command = find_command(param | ((inst & 0xf) << 8));
+				mused.song.pattern[pat].step[s].command = find_command_pt(param | ((inst & 0xf) << 8));
 				++pat;
 			}
 		
@@ -178,20 +246,140 @@ static int import_mod(FILE *f)
 }
 
 
+static int import_ahx(FILE *f)
+{
+	char sig[4];
+	
+	fread(sig, 1, sizeof(sig), f);
+	
+	int ver = 0;
+	
+	if (memcmp("THX\0", sig, 4) == 0)
+	{
+		ver = 0;
+	}
+	else if (memcmp("THX\1", sig, 4) == 0)
+	{
+		ver = 1;
+	}
+	else return 0;
+	
+	Uint8 byte;
+	Uint16 word;
+	
+	fread(&word, 1, sizeof(word), f); // title offset
+	fread(&word, 1, sizeof(word), f); 
+	
+	word = SDL_SwapBE16(word);
+	
+	debug("word = %x", word);
+	
+	int track0 = (word & 0x8000) != 0;
+	
+	mused.song.song_rate = (1 + ((word & 0x7000) >> 12)) * 50;
+	
+	int LEN = word & 0xfff;
+	
+	fread(&word, 1, sizeof(word), f); 
+	
+	mused.song.loop_point = SDL_SwapBE16(word);
+	
+	fread(&byte, 1, sizeof(byte), f); 
+	
+	int TRL = mused.sequenceview_steps = byte;
+	
+	mused.song.loop_point *= TRL;
+	mused.song.song_length = LEN * TRL;
+	
+	fread(&byte, 1, sizeof(byte), f); 
+	
+	int TRK = byte;
+	
+	fread(&byte, 1, sizeof(byte), f);  
+	
+	int SMP = byte;
+	
+	fread(&byte, 1, sizeof(byte), f); 
+	
+	int SS = byte;
+	
+	fseek(f, SS * 2, SEEK_CUR); // we don't need the subsong positions
+	
+	debug("LEN = %d TRL = %d TRK = %d SMP = %d SS = %d", LEN, TRL, TRK, SMP, SS);
+	
+	for (int i = 0 ; i < LEN ; ++i)
+	{
+		for (int c = 0 ; c < 4 ; ++c)
+		{
+			Uint8 pat;
+			Sint8 trans;
+			fread(&pat, 1, 1, f);
+			fread(&trans, 1, 1, f);
+			
+			if (!track0 && pat == 0)
+				continue;
+			
+			add_sequence(c, i * TRL, pat - !track0, trans);
+		}	
+	}
+	
+	for (int pat = 0 ; pat < TRK ; ++pat)
+	{
+		resize_pattern(&mused.song.pattern[pat], TRL);
+		
+		for (int s = 0 ; s < TRL ; ++s)
+		{
+			Uint32 step = 0;
+			fread((Uint8*)&step + 1, 3, 1, f);
+			
+			step = SDL_SwapBE32(step);
+			
+			Uint8 note = (step >> 18) & 0x3f;
+			Uint8 instrument = (step >> 12) & 0x3f;
+			Uint8 command = (step >> 8) & 0xf;
+			Uint8 data = step & 0xff;
+			
+			mused.song.pattern[pat].step[s].note = note ? (note - 1) + 12 : MUS_NOTE_NONE;
+			mused.song.pattern[pat].step[s].instrument = instrument ? instrument - 1 : MUS_NOTE_NO_INSTRUMENT;
+			mused.song.pattern[pat].step[s].ctrl = 0;
+			mused.song.pattern[pat].step[s].command = find_command_ahx(command, data, &mused.song.pattern[pat].step[s].ctrl);
+		}
+	}
+	
+	return 1;
+}
+
+
 void import_module(void *type, void* unused1, void* unused2)
 {
 	int r = confirm_ync(domain, mused.slider_bevel, &mused.largefont, "Save song?");
 				
 	if (r == 0) return;
 	if (r == 1) { change_mode(EDITSEQUENCE); if (!save_data()) return; }
+	
+	static const char *mod_name[] = {"a Protracker", "an AHX"};
+	static const char *mod_ext[] = {"mod", "ahx"};
+	
+	char buffer[100];
+	snprintf(buffer, sizeof(buffer), "Import %s module", mod_name[(int)type]);
 
-	FILE * f = open_dialog("rb", "Import module", "mod", domain, mused.slider_bevel, &mused.largefont, &mused.smallfont);
+	FILE * f = open_dialog("rb", buffer, mod_ext[(int)type], domain, mused.slider_bevel, &mused.largefont, &mused.smallfont);
 	
 	if (!f) return;
 	
 	new_song();
 	
-	if (!import_mod(f)) msgbox(domain, mused.slider_bevel, &mused.largefont, "Not a Protracker module", MB_OK);
+	switch ((int)type)
+	{
+		case IMPORT_MOD: r = import_mod(f); break;
+		case IMPORT_AHX: r = import_ahx(f); break;
+	}
+	
+	if (!r) 
+	{
+		snprintf(buffer, sizeof(buffer), "Not %s module", mod_name[(int)type]);
+		msgbox(domain, mused.slider_bevel, &mused.largefont, buffer, MB_OK);
+	}
 	
 	fclose(f);
 }
