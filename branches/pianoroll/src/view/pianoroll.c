6ab5cc41251d858a25805bb7d582e2e27d5d3b57
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2010-03-19 11:56:26 +0000

    Added quarter frequency flag for bass instruments

--- branches/pianoroll/src/view/pianoroll.c
+++ branches/pianoroll/src/view/pianoroll.c
@@ -7,17 +7,25 @@
 
 extern Mused mused;
 
-void pianoroll_view(SDL_Surface *screen, const SDL_Rect *area, const SDL_Event *event, void *param)
+void pianoroll_view(SDL_Surface *screen, const SDL_Rect *_area, const SDL_Event *event, void *param)
 {
-	SDL_Rect r;
-	copy_rect(&r, area);
-	SDL_FillRect(screen, &r, 0);
+	SDL_Rect r, area;
+	copy_rect(&area, _area);
+	copy_rect(&r, &area);
+	
+	area.h = FREQ_TAB_SIZE * 2 + 4;
+	
+	bevel(mused.screen, &area, mused.slider_bevel, BEV_SEQUENCE_BORDER);
+	adjust_rect(&area, 2); 
+	
 	
 	static const Uint32 pal[6] = { 0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00c0, 0x808080 };
 
 	for (int c = 0 ; c < mused.song.num_channels ; ++c)
 	{
 		struct { SDL_Rect area; Uint8 inst; } note;
+		
+		memset(&note, 0, sizeof(note));
 	
 		for (int s = 0; s < mused.song.num_sequences[c] ; ++s)
 		{	
@@ -32,8 +40,11 @@ void pianoroll_view(SDL_Surface *screen, const SDL_Rect *area, const SDL_Event *
 					if (note.area.w > 0)
 						SDL_FillRect(screen, &note.area, pal[note.inst % 6]);
 						
-					note.area.x = (area->x + mused.song.sequence[c][s].position + ps) * 2 - mused.pianoroll_x_position * 2;
-					note.area.y = (area->y + (FREQ_TAB_SIZE - 1 - (mused.song.sequence[c][s].note_offset + pat->step[ps].note + mused.song.instrument[inst].base_note - MIDDLE_C))) * 2 - mused.pianoroll_y_position * 2;
+					note.area.x = (area.x + mused.song.sequence[c][s].position + ps) * 2 - mused.pianoroll_x_position * 2;
+					
+					if (note.area.x >= area.x + area.w) goto out;
+					
+					note.area.y = (area.y + (FREQ_TAB_SIZE - 1 - (mused.song.sequence[c][s].note_offset + pat->step[ps].note + mused.song.instrument[inst].base_note - MIDDLE_C))) * 2 - mused.pianoroll_y_position * 2;
 					note.area.w = 2;
 					note.area.h = 2;
 					note.inst = inst;
@@ -52,20 +63,22 @@ void pianoroll_view(SDL_Surface *screen, const SDL_Rect *area, const SDL_Event *
 		
 		if (note.area.w > 0)
 			SDL_FillRect(screen, &note.area, pal[note.inst % 6]);
+			
+		out:;
 	}
 	
 	if (mused.flags & SONG_PLAYING)
 	{
 		int x = (mused.stat_song_position - mused.pianoroll_x_position) * 2;
 		
-		if (x > 0 && x < area->w)
+		if (x > 0 && x < area.w)
 		{
-			SDL_Rect pos = {x, 0, 2, area->h};
+			SDL_Rect pos = {area.x + x, area.y, 2, area.h};
 			bevel(mused.screen, &pos, mused.slider_bevel, BEV_SEQUENCE_PLAY_POS_HORIZ);
 		}
 	}
 	
-	slider_set_params(&mused.pianoroll_x_param, 0, my_max(0, (int)mused.song.song_length - 1), mused.pianoroll_x_position, mused.pianoroll_x_position + area->w / 2 - 1, &mused.pianoroll_x_position, 1, SLIDER_HORIZONTAL, mused.slider_bevel);
-	slider_set_params(&mused.pianoroll_y_param, 0, FREQ_TAB_SIZE - 1, mused.pianoroll_y_position, mused.pianoroll_y_position + area->h / 2 - 1, &mused.pianoroll_y_position, 1, SLIDER_VERTICAL, mused.slider_bevel);
+	slider_set_params(&mused.pianoroll_x_param, 0, my_max(0, (int)mused.song.song_length - 1), mused.pianoroll_x_position, mused.pianoroll_x_position + area.w / 2 - 1, &mused.pianoroll_x_position, 1, SLIDER_HORIZONTAL, mused.slider_bevel);
+	slider_set_params(&mused.pianoroll_y_param, 0, FREQ_TAB_SIZE - 1, mused.pianoroll_y_position, my_min(FREQ_TAB_SIZE, mused.pianoroll_y_position + area.h / 2) - 1, &mused.pianoroll_y_position, 1, SLIDER_VERTICAL, mused.slider_bevel);
 	
 }
