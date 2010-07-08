90abbb2bcd671cdb5c7979e523f5cf5ecebd95be
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2010-07-08 14:13:33 +0000

    Edit mode enable by default

--- trunk/src/mused.c
+++ trunk/src/mused.c
@@ -234,7 +234,7 @@ void init(MusInstrument *instrument, MusPattern *pattern, MusSeqPattern sequence
 	
 	memset(&mused, 0, sizeof(mused));
 	
-	mused.flags = MULTICHANNEL_PREVIEW|ANIMATE_CURSOR;
+	mused.flags = MULTICHANNEL_PREVIEW|ANIMATE_CURSOR|EDIT_MODE;
 	mused.screen = screen;
 	mused.done = 0;
 	mused.octave = 4;
