62f19e910146fd37fb8d328bd251c11fc5445682
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2010-12-10 15:25:24 +0000

    Enabling edit mode with space should stop playing

--- src/event.c
+++ src/event.c
@@ -1105,6 +1105,7 @@ void pattern_event(SDL_Event *e)
 			{
 				if (e->key.keysym.sym == SDLK_SPACE)
 				{
+					if (mused.flags & SONG_PLAYING) stop(0, 0, 0);
 					mused.flags ^= EDIT_MODE;
 					play_the_jams(e->key.keysym.sym, -1);
 				}
