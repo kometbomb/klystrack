47f1245e4309f91592224371ebdc4e30b2461179
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2011-08-14 16:55:18 +0000

    Console init from bundle instead of path

--- trunk/src/console.h
+++ trunk/src/console.h
@@ -51,7 +51,7 @@ void console_set_background(Console * c, int enabled);
 void console_reset_cursor(Console * c);
 void console_set_clip(Console * c, const SDL_Rect *rect);
 void console_clear(Console *console);
-Console * console_create(const char * theme);
+Console * console_create(Bundle *b);
 void console_set_color(Console* console, Uint32 color, int flags);
 const SDL_Rect * console_write(Console* console, const char *string);
 const SDL_Rect * console_write_args(Console* console, const char *string, ...)  __attribute__ ((format (printf, 2, 3)));
