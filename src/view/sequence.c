3df3a3ee784a928257526c0e4a13f2133a272180
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2011-11-17 18:52:51 +0000

    Horiz sequence scroll didn't work

--- src/view/sequence.c
+++ src/view/sequence.c
@@ -18,14 +18,15 @@ void sequence_view2(SDL_Surface *dest_surface, const SDL_Rect *_dest, const SDL_
 	
 	slider_set_params(&mused.sequence_slider_param, 0, mused.song.song_length - 1, my_max(0, top), bottom - mused.sequenceview_steps, &mused.sequence_position, mused.sequenceview_steps, SLIDER_VERTICAL, mused.slider_bevel->surface);
 	
-	const int w = my_max(dest.w / mused.song.num_channels - 1, 99);
+	const int w = my_max(dest.w / mused.song.num_channels - 1, 64);
 	
-	slider_set_params(&mused.sequence_horiz_slider_param, 0, mused.song.num_channels - 1, mused.sequence_horiz_position, my_min(mused.song.num_channels - 1, mused.sequence_horiz_position + dest.w / w), &mused.sequence_horiz_position, mused.sequenceview_steps, SLIDER_VERTICAL, mused.slider_bevel->surface);
+	slider_set_params(&mused.sequence_horiz_slider_param, 0, mused.song.num_channels - 1, mused.sequence_horiz_position, my_min(mused.song.num_channels - 1, mused.sequence_horiz_position + (dest.w - (w - 1)) / (w + 1)), 
+		&mused.sequence_horiz_position, 1, SLIDER_HORIZONTAL, mused.slider_bevel->surface);
 		
 	for (int channel = mused.sequence_horiz_position ; channel < mused.song.num_channels ; ++channel)
 	{
 		const MusSeqPattern *sp = &mused.song.sequence[channel][0];
-		const int x = channel * (w + 1);
+		const int x = (channel - mused.sequence_horiz_position) * (w + 1);
 		
 		for (int i = 0 ; i < mused.song.num_sequences[channel] ; ++i, ++sp)
 		{
@@ -46,7 +47,7 @@ void sequence_view2(SDL_Surface *dest_surface, const SDL_Rect *_dest, const SDL_
 			
 			SDL_SetClipRect(mused.screen, &dest);
 			
-			bevel(mused.screen, &pat, mused.slider_bevel->surface, BEV_THIN_FRAME);
+			bevel(mused.screen, &pat, mused.slider_bevel->surface, BEV_PATTERN);
 			
 			adjust_rect(&text, 2);
 			
@@ -56,15 +57,36 @@ void sequence_view2(SDL_Surface *dest_surface, const SDL_Rect *_dest, const SDL_
 		}
 	}
 	
+	SDL_SetClipRect(mused.screen, &dest);
+	
+	SDL_Rect loop = { dest.x, (mused.song.loop_point - top) * height / mused.sequenceview_steps + dest.y, dest.w, (mused.song.song_length - mused.song.loop_point) * height / mused.sequenceview_steps };
+	bevel(mused.screen, &loop, mused.slider_bevel->surface, BEV_SEQUENCE_LOOP);
+	
 	if (mused.focus == EDITSEQUENCE)
 	{
-		SDL_Rect pat = { mused.current_sequencetrack * w + dest.x, (mused.current_sequencepos - top) * height / mused.sequenceview_steps + dest.y, w, height };
+		SDL_Rect pat = { (mused.current_sequencetrack - mused.sequence_horiz_position) * (w + 1) + dest.x, (mused.current_sequencepos - top) * height / mused.sequenceview_steps + dest.y, w, height };
 			
 		clip_rect(&pat, &dest);
 		adjust_rect(&pat, -2);
 		
 		set_cursor(&pat);
+		
+		if (mused.selection.start != mused.selection.end)
+		{
+			if (mused.selection.start <= bottom && mused.selection.end >= top)
+			{
+				SDL_Rect selection = { dest.x + (w + 1) * (mused.current_sequencetrack - mused.sequence_horiz_position), 
+					dest.y + height * (mused.selection.start - mused.sequence_position) / mused.sequenceview_steps, w, height * (mused.selection.end - mused.selection.start) / mused.sequenceview_steps};
+					
+				adjust_rect(&selection, -3);
+				bevel(mused.screen, &selection, mused.slider_bevel->surface, BEV_SELECTION);
+			}
+		}
 	}
 	
 	SDL_SetClipRect(mused.screen, NULL);
+	
+	SDL_Rect scrollbar = { _dest->x, _dest->y + _dest->h - SCROLLBAR, _dest->w, SCROLLBAR };
+	
+	slider(dest_surface, &scrollbar, event, &mused.sequence_horiz_slider_param); 
 }
