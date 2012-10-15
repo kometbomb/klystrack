163a4dda4728217d58defb34cb276f6afb6e5ed1
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2012-10-15 19:16:15 +0000

    Missing file

---
+++ trunk/src/wave_action.h
@@ -0,0 +1,7 @@
+#ifndef __WAVE_ACTION_H
+#define __WAVE_ACTION_H
+
+void wavetable_drop_lowest_bit(void *unused1, void *unused2, void *unused3);
+void wavetable_halve_samplerate(void *unused1, void *unused2, void *unused3);
+
+#endif
