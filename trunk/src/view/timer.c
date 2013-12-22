9d8abb2ed0553c7357aded4b9f9cc3bf5b44db3a
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2013-12-22 14:27:56 +0000

    Play time display/timer

---
+++ trunk/src/view/timer.c
@@ -0,0 +1,18 @@
+#include "timer.h"
+#include "gui/bevel.h"
+#include "mused.h"
+#include "mybevdefs.h"
+
+void timer_view(SDL_Surface *dest_surface, const SDL_Rect *dest, const SDL_Event *event, void *param)
+{
+	//bevel(mused.screen, dest, mused.slider_bevel->surface, BEV_THIN_FRAME);
+	
+	SDL_Rect field;
+	copy_rect(&field, dest);
+	adjust_rect(&field, 2);
+	
+	if (mused.flags & SONG_PLAYING)
+		font_write_args(&mused.smallfont, mused.screen, &field, "%02d:%02d", mused.time_played / 60, mused.time_played % 60);
+	else
+		font_write(&mused.smallfont, mused.screen, &field, "00:00");
+}
