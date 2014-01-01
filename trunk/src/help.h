0e463091909fd45ceefcdac478cab401f5550407
kometbomb@gmail.com <kometbomb@gmail.com@33023d1e-a57b-11de-b1fe-bba1d6bec496>
2014-01-01 17:51:00 +0000

    Forgot the damn files

---
+++ trunk/src/help.h
@@ -0,0 +1,9 @@
+#ifndef HELP_H
+#define HELP_H
+
+#include "mused.h"
+#include "gfx/gfx.h"
+
+int helpbox(const char *title, GfxDomain *domain, SDL_Surface *gfx, const Font *largefont, const Font *smallfont);
+
+#endif
