3be32a6ddd4cac3c8ed10be16199ee0b81962d7c
Tero Lindeman <kometbomb@kometbomb.net>
2015-04-16 19:38:16 +0300

    WAV export now uses WaveWriter

--- src/export.c
+++ src/export.c
@@ -34,6 +34,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include "gfx/font.h"
 #include "theme.h"
 #include <string.h>
+#include "wavewriter.h"
 
 extern GfxDomain *domain;
 
@@ -56,68 +57,9 @@ void export_wav(MusSong *song, CydWavetableEntry * entry, FILE *f)
 	const int channels = 2;
 	Sint16 buffer[2000 * channels];
 	
-	Uint32 tmp = 0;
-	
-	fwrite("RIFF", 4, 1, f);
-	
-	Uint32 riffsize = ftell(f);
-	
-	FIX_ENDIAN(riffsize);
-	
-	fwrite(&tmp, 4, 1, f);
-	fwrite("WAVE", 4, 1, f);
-	fwrite("fmt ", 4, 1, f);
-	
-	tmp = 16;
-	
-	FIX_ENDIAN(tmp);
-	
-	fwrite(&tmp, 4, 1, f);
-	
-	tmp = 1;
-	
-	FIX_ENDIAN((*(Uint16*)&tmp));
-	
-	fwrite(&tmp, 2, 1, f);
-	
-	tmp = channels;
-	
-	FIX_ENDIAN((*(Uint16*)&tmp));
-	
-	fwrite(&channels, 2, 1, f);
-	
-	tmp = 44100;
-	
-	FIX_ENDIAN(tmp);
-	
-	fwrite(&tmp, 4, 1, f);
-	
-	tmp = 44100 * channels * sizeof(buffer[0]);
-	
-	FIX_ENDIAN(tmp);
-	
-	fwrite(&tmp, 4, 1, f);
-	
-	tmp = channels * sizeof(buffer[0]);
-	
-	FIX_ENDIAN((*(Uint16*)&tmp));
-	
-	fwrite(&tmp, 2, 1, f);
-	
-	tmp = 16;
-	
-	FIX_ENDIAN((*(Uint16*)&tmp));
-	
-	fwrite(&tmp, 2, 1, f);
-	
-	fwrite("data", 4, 1, f);
-	
-	size_t chunksize = ftell(f);
-	
-	tmp = 0;
-	fwrite(&tmp, 4, 1, f);
-	
 	int last_percentage = -1;
+	
+	WaveWriter *ww = ww_create(f, cyd.sample_rate, 2);
 	
 	for (;;)
 	{
@@ -125,7 +67,7 @@ void export_wav(MusSong *song, CydWavetableEntry * entry, FILE *f)
 		cyd_output_buffer_stereo(&cyd, (Uint8*)buffer, sizeof(buffer));
 		
 		if (cyd.samples_output > 0)
-			fwrite(buffer, cyd.samples_output * channels * sizeof(buffer[0]), 1, f);
+			ww_write(ww, buffer, cyd.samples_output);
 		
 		if (mus.song_position >= song->song_length) break;
 		
@@ -174,20 +116,7 @@ void export_wav(MusSong *song, CydWavetableEntry * entry, FILE *f)
 	
 abort:;
 	
-	Uint32 sz = ftell(f) - 8;
-	
-	FIX_ENDIAN(sz);
-	
-	fseek(f, riffsize, SEEK_SET);
-	fwrite(&sz, sizeof(sz), 1, f);
-	
-	sz = sz + 8 - (chunksize + 4);
-	
-	fseek(f, chunksize, SEEK_SET);
-	
-	FIX_ENDIAN(sz);
-	
-	fwrite(&sz, sizeof(sz), 1, f);
+	ww_finish(ww);
 	
 	cyd.wavetable_entries = prev_entry;
 	
