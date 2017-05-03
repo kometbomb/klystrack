43974500248659971d3b1047a14e44e45796579c
Tero Lindeman <kometbomb@kometbomb.net>
2017-05-03 21:39:23 +0300

    Use klystron_version.h instead of version.h
    Avoids filename clash

--- src/action.c
+++ src/action.c
@@ -31,7 +31,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include "event.h"
 #include "gui/msgbox.h"
 #include "version.h"
-#include "../../klystron/src/version.h"
+#include "klystron_version.h"
 #include "gfx/gfx.h"
 #include "theme.h"
 #include "key.h"
